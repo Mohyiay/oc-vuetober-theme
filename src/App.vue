991ff83253e1e080096a72a17d9adb3b48690b71
scottbedard <scottbedard90@gmail.com>
2018-02-21 23:20:02 -0700

    get tailwind wired up

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <h1 class="bg-orange rounded shadow text-center">hello world</h1>
   </div>
 </template>
 
@@ -11,13 +10,4 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/main.css"></style>
