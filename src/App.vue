4afdd5a99461318e9883597a61a34a1e1ccda538
scottbedard <scottbedard90@gmail.com>
2018-02-23 01:20:47 -0700

    playing with tailwind a bit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
+<!-- global styles -->
+<style lang="scss" src="../scss/global.scss"></style>
+
+<!-- tailwind -->
+<style src="./assets/main.css"></style>
+
 <template>
-  <div id="app">
-    <h1 class="text-red mb-4">hello world</h1>
-    <Foo />
-  </div>
+    <div id="app" class="h-full bg-grey-lighter flex items-center justify-center">
+        <div class="bg-white p-4 rounded shadow-sm">
+            <h1 class="font-thin">Welcome to your new Vuetober theme</h1>
+            <div class="text-center mt-4">
+                <a href="javascript:;">Getting started</a>
+            </div>
+        </div>
+    </div>
 </template>
 
-<script>
-import Foo from './foo';
-
-export default {
-  components: { Foo },
-  name: 'App',
-};
-</script>
-
-<style src="./assets/main.css"></style>
